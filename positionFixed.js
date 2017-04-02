d46f702ef5db62d32ac86a884ee36f9ac871a0f6
SeanTaylorLane <seantlane1999@gmail.com>
2017-04-01 20:30:00 -0400

    CSS and positionFixed.js automization

--- positionFixed.js
+++ positionFixed.js
@@ -4,5 +4,8 @@ function menuFix() {
     
     notMenu.style.marginTop = menuHeight + "px";
     
-   // console.log(menuHeight);
+    console.log(menuHeight);
+    console.log("dicks"); //easter egg!
 }
+
+document.getElementsByTagName("body")[0].addEventListener("load", menuFix, true); // Not sure why capturing works and bubbling doesn't even call the event handler... whatever I'll look it up later
